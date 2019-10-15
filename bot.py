86d5fb88a00cd636efde605a16694bf73235a17d
zhuchkovap <52775343+zhuchkovap@users.noreply.github.com>
2019-10-15 19:43:31 +0500

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('776550937:AAELEr0c3H6dM-9QnlDD-0Q0Fcd65pPyAiM')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-
-bot.polling()
